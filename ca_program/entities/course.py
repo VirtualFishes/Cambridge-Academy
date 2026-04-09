14e0de533a90b4fd94cf99532b32479a45a1e65f
stivenmejia1 <mejiajunior081@gmail.com>
2026-04-09 12:08:46 -0500

    borrando coursos de HU07 ya que era ua copia del 06 para corregir mi error de combiacion

--- ca_program/entities/course.py
+++
@@ -1,8 +0,0 @@
-class Course:
-    def __init__(self, course_name, code_course, quantity):
-        self.course_name = course_name
-        self.code_course = code_course
-        self.quantity = quantity
-
-    def __str__(self):
-        return f"{self.course_name} | Código: {self.code_course} | Cantidad: {self.quantity}"
